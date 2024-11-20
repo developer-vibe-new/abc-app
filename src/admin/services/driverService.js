f3aca8dc0ad5914f8d23b8185387b902e56518be
rishikaImg <rishikamandal.img@gmail.com>
2024-11-20 15:58:48 +0530

    driver

--- src/admin/services/driverService.js
+++ src/admin/services/driverService.js
@@ -5,9 +5,9 @@ const mongoose = require('mongoose');
 
 exports.driverCreate = async (req) => {
     try {
-        const { first_name, last_name, full_name, mobile, email, type } = req.body;
+        const { first_name, last_name, mobile, email, type } = req.body;
         const image = `${req.body.typeName}/${req.file.filename}`;
-        if (!first_name || !last_name || !full_name || !mobile || !email || !type || !image) {
+        if (!first_name || !last_name || !mobile || !email || !type || !image) {
             return {
                 statusCode: statusCode.BAD_REQUEST,
                 success: false,
@@ -15,7 +15,7 @@ exports.driverCreate = async (req) => {
             };
         }
         const createdData = await driverModel.create({
-            first_name: first_name, last_name: last_name, full_name: full_name, mobile: mobile, email: email, type: type, image: image
+            first_name: first_name, last_name: last_name, mobile: mobile, email: email, type: type, image: image
         });
         return {
             statusCode: statusCode.OK,
@@ -114,7 +114,7 @@ exports.driverView = async (req) => {
                 $project: {
                     first_name: 1,
                     last_name: 1,
-                    full_name: 1,
+
                     image: 1,
                     email: 1,
                     mobile: 1,
@@ -305,7 +305,7 @@ exports.blockedDriverList = async (req) => {
                 $project: {
                     first_name: 1,
                     last_name: 1,
-                    full_name: 1,
+
                     image: 1,
                     email: 1,
                     mobile: 1,
@@ -401,6 +401,121 @@ exports.unblockDriver = async (req) => {
         console.log(error);
         return {
             statusCode: statusCode.BAD_REQUEST,
+            success: false,
+            message: resMessage.Internal_Server_Error,
+            error: error.message || "Internal Server Error",
+        };
+    }
+};
+
+exports.driverView = async (req) => {
+    try {
+        var page = req.query.page || 1;
+        let pagesize = req.query.pagesize || 10;
+
+        let search_value = req.query.search || "";
+        var conditions = [];
+
+        if (search_value) {
+            conditions.push({
+                $match: {
+                    first_name: { $regex: search_value, $options: "i" }
+                }
+            });
+        }
+        if (req.query.kycStatus) {
+
+            if (req.query.kycStatus == "Complete") {
+                conditions.push({
+                    $match: {
+                        kycStatus: 1
+                    }
+                });
+            }
+            if (req.query.kycStatus == "Pending") {
+                conditions.push({
+                    $match: {
+                        kycStatus: 0
+                    }
+                });
+            }
+            if (req.query.kycStatus == "Not Uploaded") {
+                conditions.push({
+                    $match: {
+                        kycStatus: -1
+                    }
+                });
+            }
+        }
+        if (req.query.vehicleStatus) {
+
+            if (req.query.vehicleStatus == "Pending") {
+                conditions.push({
+                    $match: {
+                        vehicleStatus: 0
+                    }
+                });
+            }
+            if (req.query.vehicleStatus == "Complete") {
+                conditions.push({
+                    $match: {
+                        vehicleStatus: 1
+                    }
+                });
+            }
+        }
+
+        conditions.push({
+            $match: {
+                status: "Unblock",
+                is_delete: false
+            }
+        });
+
+        conditions.push({
+            $addFields:
+            {
+                image: {
+                    $concat: [
+                        "http://192.168.0.18:6161/",
+                        "$image"
+                    ]
+                }
+
+            }
+        },
+            {
+                $project: {
+                    first_name: 1,
+                    last_name: 1,
+
+                    image: 1,
+                    email: 1,
+                    mobile: 1,
+                    balance: 1,
+                    is_online: 1,
+                    kycStatus: 1,
+                    vehicleStatus: 1,
+                    status: 1,
+                    pending_amount: 1
+                }
+            });
+        conditions.push(
+            { $sort: { first_name: 1 } },
+            { $skip: (page - 1) * pagesize },
+            { $limit: pagesize }
+        );
+        const viewAllData = await driverModel.aggregate(conditions);
+
+        return {
+            statusCode: statusCode.OK,
+            success: true,
+            message: resMessage.Data_Fetch_Successfully,
+            data: viewAllData
+        };
+    } catch (error) {
+        console.log(error);
+        return {
             success: false,
             message: resMessage.Internal_Server_Error,
             error: error.message || "Internal Server Error",
