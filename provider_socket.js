94e6c2eb81e51c3b75d25d5078e0ed6f5c54e0fe
rinku <rinku@imgglobalinfotech.com>
2025-06-24 17:32:09 +0530

    res

--- provider_socket.js
+++ provider_socket.js
@@ -859,7 +859,7 @@ async function runServer() {
                 delete socket.ride_details;
                 const ride = ride_details.toObject();
                 const user_socket = await client.get("socket_user:" + ride.basic.user_id.toString());
-
+                console.log('user_socket', user_socket);
                 if (user_socket) {
                   socket.to(user_socket).emit('ride_finished', {
                     ride_id,
